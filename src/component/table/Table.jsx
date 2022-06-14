c5d61f87ef8ec59671322610a41176f901071439
sarpongduff <bernardsarpong@Bernards-MacBook-Pro.local>
2022-06-14 01:30:14 -0700

    Adding tab selection feature and submit tab on the grid

--- src/component/table/Table.jsx
+++ src/component/table/Table.jsx
@@ -1,28 +1,57 @@
 import './table.scss'
 import { DataGrid } from '@mui/x-data-grid';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70},
-  { field: 'firstName', headerName: 'First Name', width: 130 },
-  { field: 'lastName', headerName: 'Last Name', width: 130 },
-  { field: 'branch', headerName: 'Branch', width: 150 },
+// const columns = [
+//   { field: 'id', headerName: 'ID', width: 70},
+//   { field: 'firstName', headerName: 'First Name', width: 130 },
+//   { field: 'lastName', headerName: 'Last Name', width: 130 },
+//   { field: 'branch', headerName: 'Branch', width: 150 },
 
 
-  {field: 'gatheringAttd', headerName: 'Gathering', type: 'number',width: 110},
-  {field: 'impartationAttd', headerName: 'Impartation', type: 'number',width: 110},
-  {field: 'flowPrayerAttd', headerName: 'Flow Prayer', type: 'number',width: 110},
-  {field: 'tithers', headerName: 'Tithers', type: 'number',width: 110},
+//   {field: 'gatheringAttd', headerName: 'Gathering', type: 'number',width: 110},
+//   {field: 'impartationAttd', headerName: 'Impartation', type: 'number',width: 110},
+//   {field: 'flowPrayerAttd', headerName: 'Flow Prayer', type: 'number',width: 110},
+//   {field: 'tithers', headerName: 'Tithers', type: 'number',width: 110},
 
 
-  {
-    field: 'date',
-    headerName: 'Date',
-    type: 'number',
-    width: 120,
-  },
+//   {
+//     field: 'date',
+//     headerName: 'Date',
+//     type: 'number',
+//     width: 120,
+//   },
 
 
-];
+// ];
+
+let columns = [];
+
+const colGathering = [
+  { field: 'id', headerName: 'ID', width: 80},
+  { field: 'firstName', headerName: 'First Name', width: 200 },
+  { field: 'lastName', headerName: 'Last Name', width: 200 },
+  { field: 'branch', headerName: 'Branch', width: 200 },
+  {field: 'gatheringAttd', headerName: 'Attendance', type: 'number',width: 200},
+  { field: 'date', headerName: 'Date', type: 'number', width: 150 }
+]
+
+const colImpartation = [
+  { field: 'id', headerName: 'ID', width: 80},
+  { field: 'firstName', headerName: 'First Name', width: 200 },
+  { field: 'lastName', headerName: 'Last Name', width: 200 },
+  { field: 'branch', headerName: 'Branch', width: 200 },
+  {field: 'impartationAttd', headerName: 'Attendance', type: 'number',width: 200},
+  { field: 'date', headerName: 'Date', type: 'number', width: 150 }
+]
+
+const colFlowPrayer = [
+  { field: 'id', headerName: 'ID', width: 80},
+  { field: 'firstName', headerName: 'First Name', width: 200 },
+  { field: 'lastName', headerName: 'Last Name', width: 200 },
+  { field: 'branch', headerName: 'Branch', width: 200 },
+  {field: 'flowPrayerAttd', headerName: 'Attendance', type: 'number',width: 200},
+  { field: 'date', headerName: 'Date', type: 'number', width: 150 },
+]
 
 const rows = [
   {id: 1, lastName: 'Daniel', firstName: 'Odi', branch: 'Grand Rapids', gatheringAttd: 35, impartationAttd: 50,flowPrayerAttd:50, tithers:100, date:'03-23-2021'},
@@ -46,7 +75,22 @@ const rows = [
 //   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
 //   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
-export default function table() {
+
+function serviceSelection(selection) {
+  if (selection == "flowPrayer") {
+    columns = colFlowPrayer;
+  } else if (selection == "impartation") {
+    columns = colImpartation;
+  }
+  else {
+    columns = colGathering;
+  }
+  return columns
+}
+
+export default function table(props) {
+  serviceSelection(props.name);
+
   return (
     <div className='table'>
       <div className='rows' style={{ height: 400, width: '100%' }}>
