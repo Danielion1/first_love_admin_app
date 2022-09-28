df39ce3d942397b87543c83bf6c0d75100e87964
myProjectDir <danielhn37@gmail.com>
2022-09-27 23:03:22 -0400

    updated the bar chart

--- src/component/chart/Chart.jsx
+++ src/component/chart/Chart.jsx
@@ -1,107 +1,95 @@
 import './chart.scss'
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
-import { scaleOrdinal } from 'd3-scale';
-import { schemeCategory10 } from 'd3-scale-chromatic';
-import PropTypes from 'prop-types';
-
-
-const colors = scaleOrdinal(schemeCategory10).range();
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from "recharts";
 
 const data = [
-    {
-      name: 'JAN',
-      num: 4000,
+  {
+    name: 'JAN',
+    Gathering_Service: 4000,
+  },
+  {
+    name: 'FEB',
+    Gathering_Service: 3000,
+  },
+  {
+    name: 'MAR',
+    Gathering_Service: 2000,
+  },
+  {
+    name: 'APR',
+    Gathering_Service: 2780,
+  },
+  {
+    name: 'MAY',
+    Gathering_Service: 1890,
+  },
+  {
+    name: 'JUN',
+    Gathering_Service: 2390,
+  },
+  {
+    name: 'JUL',
+    Gathering_Service: 3490,
+  },
+  {
+      name: 'AUG',
+      Gathering_Service: 1890,
     },
     {
-      name: 'FEB',
-      num: 3000,
+      name: 'SEP',
+      Gathering_Service: 2390,
     },
     {
-      name: 'MAR',
-      num: 2000,
+      name: 'OCT',
+      Gathering_Service: 3490,
     },
     {
-      name: 'APR',
-      num: 2780,
+      name: 'NOV',
+      Gathering_Service: 4000,
     },
     {
-      name: 'MAY',
-      num: 1890,
+      name: 'DEC',
+      Gathering_Service: 5000,
     },
-    {
-      name: 'JUN',
-      num: 2390,
-    },
-    {
-      name: 'JUL',
-      num: 3490,
-    },
-    {
-        name: 'AUG',
-        num: 1890,
-      },
-      {
-        name: 'SEP',
-        num: 2390,
-      },
-      {
-        name: 'OCT',
-        num: 3490,
-      },
-      {
-        name: 'NOV',
-        num: 4000,
-      },
-      {
-        name: 'DEC',
-        num: 5000,
-      },
-  ];
+];
 
-  const getPath = (x, y, width, height) => `M${x},${y + height}
-          C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${x + width / 2}, ${y}
-          C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
-          Z`;
 
-const TriangleBar = (props) => {
-  const { fill, x, y, width, height } = props;
 
-  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-};
-
-TriangleBar.propTypes = {
-  fill: PropTypes.string,
-  x: PropTypes.number,
-  y: PropTypes.number,
-  width: PropTypes.number,
-  height: PropTypes.number,
-};
-export default function Chart({aspect, title}) {
+export default function Chart(aspect, title) {
   return (
     <div className='chart'>
-        <div className="title">{title}</div>
-       <ResponsiveContainer width="100%" aspect={aspect}>
+       {/* <div className="title">{title}</div> */}
+          <ResponsiveContainer width="100%" aspect={aspect}>
         <BarChart
           width={500}
-          height={500}
+          height={300}
           data={data}
           margin={{
-            top: 20,
+            top: 5,
             right: 30,
             left: 20,
-            bottom: 5,
+            bottom: 5
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
-          <Bar dataKey="num" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-            ))}
-          </Bar>
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Gathering_Service" fill="#8884d8" />
         </BarChart>
-      </ResponsiveContainer>
+        </ResponsiveContainer>
     </div>
-  )
+
+
+  );
 }
